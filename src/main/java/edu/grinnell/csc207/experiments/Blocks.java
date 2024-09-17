a0bffc4ed15ccecd41f2bb290c58ea1530cd602b
KHANH <khanhphuongdo28@gmail.com>
2024-09-17 15:12:09 -0500

    6

--- src/main/java/edu/grinnell/csc207/experiments/Blocks.java
+++ src/main/java/edu/grinnell/csc207/experiments/Blocks.java
@@ -97,6 +97,19 @@ public class Blocks {
     separator(pen);
     AsciiBlock.print(pen, new HorizontalCompositionTop(exes, exes));
 
+    AsciiBlock comp1 =
+      new HorizontalCompositionTop(exes, new HorizontalCompositionTop(ohs, exes));
+    AsciiBlock comp2 =
+      new HorizontalCompositionTop(new HorizontalCompositionTop(exes, ohs), exes);
+
+    separator(pen);
+    AsciiBlock.print(pen, comp1);
+    separator(pen);
+    AsciiBlock.print(pen, comp2);
+
+    AsciiBlock boxedcomp = new Boxed(new HorizontalCompositionTop(exes, ohs));
+    separator(pen);
+    AsciiBlock.print(pen, boxedcomp);
     pen.close();
   } // main(String[])
 } // class Blocks
